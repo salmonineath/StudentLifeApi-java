a5c3b9c19dd9c9d94a3a9d7aa459e509aec98916
salmonineath <salmonineath31@gmail.com>
2026-06-16 18:29:47 +0700

    fist commit

---
+++ src/main/java/com/studentlife/studentlifejava/StudentlifejavaApplication.java
@@ -0,0 +1,13 @@
+package com.studentlife.studentlifejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentlifejavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentlifejavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studentlife/studentlifejava/StudentlifejavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentlife.studentlifejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentlifejavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
